51bdfc083eaac8e30a6f00895a967d6def4dba98
Jon <20120441012yy@sina.com>
2019-04-01 17:27:48 +0800

    烦死了0.1；learn about the pytest

--- study/PyTest/test_pytest.py
+++ study/PyTest/test_pytest.py
@@ -1,67 +1,25 @@
 import pytest
 
-class calculator(object):
-
-    def add(self, a, b):
-        return a+b
-
-    def diff(self, a, b):
-        return a+b
-
-    def multible(self, a, b):
-        return a+b
-
-    def add(self, a, b):
-        return a+b
+class cal():
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def add(self):
+        return self.a+self.b
+    def mul(self):
+        return self.a*self.b
+    def div(self):
+        return self.a/self.b
+    def sub(self):
+        return self.a-self.b
 
 class Test_pytest(object):
 
-    cal = calculator()
-
-    def setup_module(module):
-        print("\n--------------------setup_module--------------------")
-
-    @pytest.fixture()
-    def sum(self):
-        print('--------------------------smtp---------------:')
-        return 1 + 2
-
-    def test_t1(self,sum):
-        result = self.cal.add(a=1, b=2)
-        assert 9 == result
-        print(result)
-        print(sum)
-        print(type(sum))
-        print('test_t1-')
-
-    def test_t2(self):
-        print('test_t2-')
-
-    def test_t3(self):
-        print('test_t3-')
-
-    def test_t4(self):
-        print('test_t4-')
-
-    def test_t5(self):
-        print('test_t5-')
-
-    def f(self):
-        print('SystemExit')
-        raise SystemExit(1)
-
-    def test_mytest(self):
-        with pytest.raises(SystemExit):
-            self.f()
-    # fixture
-    def test_tmpdir(self, tmpdir):
-        print(tmpdir)
-
-    def test_tmp_path(self, tmp_path):
-        print(tmp_path)
-
-    def test_tmp_path_factory(self, tmp_path_factory):
-        print(tmp_path_factory)
+    def test_assert(self):
+        x = 5
+        y = 6
+        assert x + 1 == y, "test passed: 5+1=6"
+        assert x == y, "test failed: 5 <> 6"
 
 
 if __name__ == '__main__':
