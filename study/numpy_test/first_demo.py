b3fca2489afbde415d2113a061b94986b3fd607e
Jon <20120441012yy@sina.com>
2019-04-15 17:02:15 +0800

    WTO 考试自学资料

--- study/numpy_test/first_demo.py
+++ study/numpy_test/first_demo.py
@@ -5,7 +5,11 @@ class MyTestCase(unittest.TestCase):
     def test_zeros(self):
         pic = imageio.imread('../pic_analysis/pics/demo_1.jpg')
         np_zeros = np.zeros(pic.shape)
-        print(np_zeros[0])
+        print(np_zeros[0][1])
+
+    def test_random(self):
+        arr = np.random.randn(10)
+        print(arr[8])
 
 
 if __name__ == '__main__':
